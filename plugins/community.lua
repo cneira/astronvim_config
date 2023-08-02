a6706964110973996d81ed64f62340227eda2559
neirac <cneirabustos@gmail.com>
2023-08-02 19:15:30 -0400

    sync up

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,10 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,55 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "ribelo/taskwarrior.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    -- or
+    config = true,
+  },
+  {
+    "tssm/nvim-snitch",
+    Event = "AstroFile",
+    lazy = false,
+  },
+  {
+    "tssm/nvim-reflex",
+    Event = "AstroFile",
+    lazy = false,
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = false },
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    Event = "AstroFile",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},  -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.presenter"] = { config = { zen_mode = "truezen" } },
+          ["core.ui"] = {},
+          ["core.export"] = {},
+          ["core.keybinds"] = {},
+          ["core.neorgcmd"] = {},
+          ["core.integrations.treesitter"] = {},
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/RoamNotes/",
+                main = "~/RoamNotes/personal",
+              },
+            },
+          },
+        },
+      }
+    end,
+    run = ":Neorg sync-parsers",
+  },
 }
